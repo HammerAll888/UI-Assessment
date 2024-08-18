c130ac7326135fabd3ae7377ac845c1c4a77aee1
HammerAll888 <andrew.coppins1@gmail.com>
2024-08-18 15:24:26 +1000

    Commented my code

--- Assets/Scripts/MenuSelection.cs
+++ Assets/Scripts/MenuSelection.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MenuSelection : MonoBehaviour
 {
+    //Loads the Playground scene when the start button is pressed
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
     }
 
+    //Quits the game when the Quit button is pressed
     public void QuitGame()
     {
         Application.Quit();
--- Assets/Scripts/ScaleDetection.cs
+++ Assets/Scripts/ScaleDetection.cs
@@ -6,25 +6,29 @@ using UnityEngine.UI;
 
 public class ScaleDetection : MonoBehaviour
 {
+    //Defining game objects
     [SerializeField] GameObject FirstPromt;
     [SerializeField] GameObject SecondPromt;
     public TextMeshProUGUI Objective;
 
+    //Detects when the player has entered a collider
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Scale")
         {
-            FirstPromt.SetActive(false);
-            SecondPromt.SetActive(true);
-            Objective.color = Color.green;
+            FirstPromt.SetActive(false); //Will turn off the "FirstPromt" game object
+            SecondPromt.SetActive(true); //Will turn on the "SecondPromt" game object
+            Objective.color = Color.green; //Will change the colour of the objective text
         }
     }
+
+    //Detects when the player has left a collider
     private void OnTriggerExit(Collider other)
     {
         if(other.tag == "Scale")
         {
-            FirstPromt.SetActive(true);
-            SecondPromt.SetActive(false);
+            FirstPromt.SetActive(true); //Will turn on the "FirstPromt" game object
+            SecondPromt.SetActive(false); //Will turn off the "SecondPromt" game object
         }
     }
 }
