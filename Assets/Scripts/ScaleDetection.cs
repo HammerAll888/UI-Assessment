ba88afa5e8d7d1216f2c1c0133b003843fa360bb
HammerAll888 <andrew.coppins1@gmail.com>
2024-08-18 14:39:17 +1000

    Made the objective change colour

--- Assets/Scripts/ScaleDetection.cs
+++ Assets/Scripts/ScaleDetection.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScaleDetection : MonoBehaviour
 {
     [SerializeField] GameObject FirstPromt;
     [SerializeField] GameObject SecondPromt;
+    public TextMeshProUGUI Objective;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Scale")
         {
             FirstPromt.SetActive(false);
             SecondPromt.SetActive(true);
+            Objective.color = Color.green;
         }
     }
     private void OnTriggerExit(Collider other)
